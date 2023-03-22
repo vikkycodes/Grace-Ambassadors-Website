530883b93d78e79473d3ab3a528a74b2c38f701a
vikkycodes <okorovictor185@gmail.com>
2023-03-22 04:47:03 +0100

    first commit

---
+++ life.js
@@ -0,0 +1,17 @@
+function show_hide() {
+  if (document.getElementById("menu").style.display === "inline") {
+    document.getElementById("menu").style.display = "none";
+  } else {
+    document.getElementById("menu").style.display = "inline";
+  }
+  return;
+}
+
+function modal() {
+  if (document.getElementById("give_modal").style.display === "inline") {
+    document.getElementById("give_modal").style.display = "none";
+  } else {
+    document.getElementById("give_modal").style.display = "inline";
+  }
+  return;
+}
